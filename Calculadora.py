a2f10e58ea5d88d4706a56431c0d285047d9ade4
Mañanas <esthermurado8@gmail.com>
2024-03-08 13:50:35 +0100

    Mis primeros programas con python

--- Calculadora.py
+++ Calculadora.py
@@ -2,39 +2,41 @@
 
 #Calculadora con 4 metodos y parametros con enteros
 #Queremos usar type() para ver como Python decide los tipos en base a los valores de inicialización que le damos
-class calculadora():
-    v1 = int(input("Ingresa primer valor: ", ))
-    op= input("Introduzca operador (+, -, /, *): ")
-    v2 = int(input("Ingresa segundo valor: ", ))
+
+class Calculadora():
 
     def suma(v1, v2):
-        print(v1 + v2)
+        return v1 + v2
     def resta(v1, v2):
-        print(v1 - v2)
+        return v1 - v2
     def multiplica(v1, v2):
-        print(v1 * v2)
+        return v1 * v2
     def divide(v1, v2):
-        print(v1 / v2)
+        return v1 / v2
+
+    # print(Calculadora.suma())
+
+    v1 = int(input("Ingresa primer valor: ", ))
+    op = input("Introduzca operador (+, -, /, *): ")
+    v2 = int(input("Ingresa segundo valor: ", ))
+
+    print("el tipo de v1 es", type(v1))
+    if (type(v1)==int):
+        print ("es igual")
 
     if(op == '+' or op == '-' or op == '/' or op == '*'):
         if (op == '+'):
-            suma(v1, v2)
+           print(suma(v1, v2))
         if(op == '-'):
-            resta(v1, v2)
+            print(resta(v1, v2))
         if (op == '*'):
-            multiplica(v1, v2)
+            print(multiplica(v1, v2))
         if (op == '/'):
-            divide(v1, v2)
+            print(divide(v1, v2))
     else:
         print("operador no válido ( ´･･)ﾉ(._.`)")
 
-
-'''Calculadora con 4 metodos y parametros con decimales
-Queremos usar type() para ver como Python decide los tipos en base a los valores de inicialización que le damos
-Escritura en fichero
-¿Cómo haría para escribir en fichero en el disco duro?
-Crear un array con números y sumar sus valores
-Hay que usar azúcar sintáctico para el bucle
+'''
 Crear dos diccionarios y unirlos
 Definir una clase estudiante, crear varios estudiantes y meterlos en una lista'''
 # str (string) en Python es inmutable ¿cómo puedo comprobar esto en un pequeño programa?
---
+++ Diccionario.py
@@ -0,0 +1,40 @@
+'''Diccionario == hashMap'''
+#Dictionaries in python
+
+class Diccionario:
+    def UnirDicts(self, d1, d2):
+        d1.update(d2)
+        return d1
+
+    def creaDict(self):
+        k = str(input("Introduzca clave (0 para salir): "))
+        v = 0
+        resu = {k: v}
+
+        while (k != '0'):
+            k = str(input("Introduzca clave: "))
+            v += 1
+            a2 = {k : v}
+            resu.update(a2)
+            print(resu)
+        return resu
+'''
+d1 = {'Alex' : 1,
+      'Elena' : 2,
+      'Markos' : 3,
+      'Andrea' : 4}
+
+d2 = {'Paula' : 5,
+      'Cris' : 6,
+      'Emma' : 7}
+'''
+
+#d1.update(d2)
+#print(d1)
+
+miDiccionario = Diccionario()
+d1 = miDiccionario.creaDict()
+d2 = miDiccionario.creaDict()
+print(miDiccionario.UnirDicts(d1, d2))
+
+
