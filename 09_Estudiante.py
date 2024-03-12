4673815edb9c8719022305cc1604c73b043ce247
Mañanas <esthermurado8@gmail.com>
2024-03-12 10:55:06 +0100

    Mejora 09, incluyendo vble global

--- 09_Estudiante.py
+++ 09_Estudiante.py
@@ -1,31 +1,42 @@
 class Estudiante():
 
-    nombre = ""
-    apellido = ""
-    edad = 0
-
     def __init__(self, nombre, apellido, edad):
         self.nombre = nombre
         self.apellido = apellido
         self.edad = edad
 
-def nuevoEstudiante():
-    nom = str(input("Nombre: "))
-    ape = str(input("Apellido: "))
-    edad = int(input("Edad: "))
-    e = Estudiante(nom, ape, edad)
-    return e
+class CreaEstudiante():
 
-def listaAlumnos():
-    q = str(input("'a' para agregar: "))
-    lista = []
-    while (q == 'a'):
-        lista.append(nuevoEstudiante())
-        q = str(input("'a' para agregar: "))
-    return lista
+    num_people = 0
 
-def imprimeLista(lista):
-    for alumno in lista:
-        print(alumno.nombre, alumno.apellido, "- ", alumno.edad)
+    def nuevoEstudiante(self):
+        nom = str(input("Nombre: "))
+        ape = str(input("Apellido: "))
+        edad = int(input("Edad: "))
+        e = Estudiante(nom, ape, edad)
+        CreaEstudiante.num_people += 1
+        return e
 
-imprimeLista(listaAlumnos())
+    def listaAlumnos(self):
+        q = str(input("'a' --> agregar | 'c' --> cantidad de alumnos | 'v' to view | 'e' to exit: "))
+        lista = []
+        while (q == 'a' or q == 'c' or q == 'v'):
+            if q == 'a':
+                lista.append(self.nuevoEstudiante())
+            if q == 'c':
+                print(CreaEstudiante().num_people)
+            if q == 'v':
+                self.imprimeLista(lista)
+            q = str(input("Introduzca comando ('a' | 'c' | 'v'): "))
+        return lista
+
+    def imprimeLista(self, lista):
+        i = 1
+        for alumno in lista:
+            print(i,"|", alumno.nombre, alumno.apellido,"-", alumno.edad)
+            i += 1
+
+
+crear = CreaEstudiante()
+alumnos = crear.listaAlumnos()
+crear.imprimeLista(alumnos)
